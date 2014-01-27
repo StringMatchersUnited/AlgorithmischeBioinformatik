286932bd4ac9d88067acc1e0b0a1a255878dfed2
Maddien <Martin.Hoffmann.1990@gmail.com>
2014-01-27 12:05:37 -0800

    to check for the correct result, given code for exam

---
+++ uebungen/src/main/java/de/huberlin/algobio/ws1314/gruppe2/eval/UsageExample.java
@@ -0,0 +1,32 @@
+package algbioinf.aufgabe6;
+
+import algbioinf.aufgabe6.ResultChecker.Result;
+
+public class UsageExample {
+
+
+	public static void main(String[] args) {
+
+		/*
+		 * Compare two result files using ResultChecker.compareResultFiles
+		 */
+		Result result = ResultChecker.compareResultFiles(args[0], args[1]);
+		
+		/*
+		 * You can output the result of the comparison this way ...
+		 */
+		System.out.println(result);
+		
+		/*
+		 * If you like to, there is a simple pass-test function, too.
+		 */
+		if(result.doesFilesContainSameContent()) {
+			System.out.println("You win.");
+		} else {
+			System.out.println("You lose.");
+		}
+		
+		
+	}
+
+}
